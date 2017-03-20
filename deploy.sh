120b8abb15f5342ed17cd9224ee104614d548f8e
Bogdan Dobrelya <bogdando@mail.ru>
2017-03-20 14:20:10 +0100

    Rework the play
    Drop ansible retries (doesn't work)
    Add ansible ssh config from oooq
    Use the same USER everywhere to fix ssh login issues
    Add a separate PLAY for undercloud deploy.
    Rework main PLAY as well.
    
    Signed-off-by: Bogdan Dobrelya <bogdando@mail.ru>

--- deploy.sh
+++ deploy.sh
@@ -12,55 +12,21 @@ CONF_PATH=${CONF_PATH:-${WORKSPACE}/fuel-devops-oooq.yaml}
 ENV_NAME=${ENV_NAME:-oooq-warp}
 SLAVES_COUNT=${SLAVES_COUNT:-0}
 IMAGE_PATH=${IMAGE_PATH:-/tmp/qs/image.qcow2}
-SSH_OPTIONS="-F /dev/null -i ${ADMIN_KEY}"
 LOG_LEVEL=${LOG_LEVEL:--v}
 ANSIBLE_TIMEOUT=${ANSIBLE_TIMEOUT:-600}
 ANSIBLE_FORKS=${ANSIBLE_FORKS:-10}
 TEARDOWN=${TEARDOWN:-true}
 
-function with_retries {
-    local retries=3
-    set +e
-    set -o pipefail
-    for try in $(seq 1 $retries); do
-        ${@}
-        [ $? -eq 0 ] && break
-        if [ "$try" = "$retries" ]; then
-            exit 1
-        fi
-    done
-    set +o pipefail
-    set -e
-}
-
 function with_ansible {
-    local tries=5
-    local retry_opt=""
-    playbook=$1
-    retryfile=${playbook/.yml/.retry}
-
-    until \
-        ANSIBLE_CONFIG=ansible.cfg \
-        ansible-playbook \
-        --ssh-extra-args "-A\ -o\ StrictHostKeyChecking=no" -u $ADMIN_USER -b \
-        --become-user=root -i ${WORKSPACE}/inventory.ini \
-        --forks=$ANSIBLE_FORKS --timeout $ANSIBLE_TIMEOUT \
-        -e ansible_ssh_user=${ADMIN_USER} \
-        -e teardown=$TEARDOWN \
-        -e @${WORKSPACE}/nodes.yaml \
-        $retry_opt $LOG_LEVEL $@; do
-            if [ $tries -gt 1 ]; then
-                tries=$((tries - 1))
-                echo "Deployment failed! Trying $tries more times..."
-            else
-                exit 1
-            fi
-
-            if test -e "$retryfile"; then
-                retry_opt="--limit @${retryfile}"
-            fi
-    done
-    rm -f "$retryfile" || true
+  ANSIBLE_CONFIG=ansible.cfg \
+  ansible-playbook \
+  -u $ADMIN_USER -b \
+  --become-user=root -i ${WORKSPACE}/inventory.ini \
+  --forks=$ANSIBLE_FORKS --timeout $ANSIBLE_TIMEOUT \
+  -e ansible_ssh_user=${ADMIN_USER} \
+  -e teardown=$TEARDOWN \
+  -e @${WORKSPACE}/nodes.yaml \
+   $LOG_LEVEL $@
 }
 
 echo "Trying to ensure bridge-nf-call-iptables is disabled..."
@@ -87,10 +53,14 @@ ansible -u ${ADMIN_USER} -i ${WORKSPACE}/inventory.ini -m ping all
 echo "Deploying with oooq"
 # FIXME(bogdando) hack a failed undercloud respinning
 if [ "${TEARDOWN}" = "false" ]; then
+  set +e
   sudo virsh destroy undercloud
   sudo virsh undefine undercloud
+  set -e
 fi
+
+# hack oooq hardocded paths
+. ${WORKSPACE}/ssh_config
+touch $SSH_CONFIG
+ln -sf $HOME $HOME/.quickstart
 with_ansible ${WORKSPACE}/oooq-warp.yaml
-#with_ansible playbooks/libvirt-teardown.yml
-#with_ansible playbooks/libvirt-setup.yml
-#...
